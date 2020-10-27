97ad09d81f133f977f6fdc2f308aeb7871786f32
Kelley van Evert <hello@kelleyvanevert.nl>
2020-10-27 11:13:14 +0100

    Solution for useToggle

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,1 +1,11 @@
-export function useToggle() {}
+import { useState, useCallback } from "react";
+
+export function useToggle(initiallyOn = false) {
+  const [on, setOn] = useState(initiallyOn);
+
+  const toggle = useCallback(() => {
+    setOn((on) => !on);
+  }, [setOn]);
+
+  return [on, toggle];
+}
