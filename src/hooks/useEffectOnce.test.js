044d38d49f575f71ecd3f0a7677f3259bbe82d46
Kelley van Evert <hello@kelleyvanevert.nl>
2020-10-27 11:24:11 +0100

    Added useEffectOnce exercise

---
+++ src/hooks/useEffectOnce.test.js
@@ -0,0 +1,24 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useEffectOnce } from "./useEffectOnce";
+
+test.skip("it runs the effect", () => {
+  const fn = jest.fn();
+
+  renderHook(() => useEffectOnce(fn));
+
+  expect(fn).toBeCalledTimes(1);
+});
+
+test.skip("it runs the effect only once", () => {
+  const fn = jest.fn();
+
+  const hook = renderHook(() => useEffectOnce(fn));
+
+  act(() => {
+    hook.rerender();
+    hook.rerender();
+    hook.rerender();
+  });
+
+  expect(fn).toBeCalledTimes(1);
+});
