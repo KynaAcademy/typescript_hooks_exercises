24091f1504206ef3ad27880e801725bc89ba20bf
Kelley van Evert <hello@kelleyvanevert.nl>
2020-10-27 12:08:53 +0100

    Added createUseValidated exercise

---
+++ src/hooks/useValidated.test.js
@@ -0,0 +1,73 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { createUseValidated } from "./useValidated";
+
+test.skip("starts out idle", () => {
+  const useValidated = createUseValidated(() => true);
+
+  const hook = renderHook(() => useValidated());
+
+  expect(hook.result.current).toBe("idle");
+});
+
+test.skip("stays idle as long as a nullish value is given", () => {
+  const useValidated = createUseValidated(() => true);
+
+  let value = undefined;
+  const hook = renderHook(() => useValidated(value));
+
+  act(() => {
+    hook.rerender();
+  });
+
+  expect(hook.result.current).toBe("idle");
+
+  act(() => {
+    value = null;
+    hook.rerender();
+  });
+
+  expect(hook.result.current).toBe("idle");
+});
+
+test.skip("starts out with correct validation", () => {
+  const useValidated = createUseValidated((value) => {
+    return value.indexOf("a") >= 0;
+  });
+
+  let value = "berate";
+  const hook = renderHook(() => useValidated(value));
+
+  expect(hook.result.current).toBe("valid");
+});
+
+test.skip("revalidates when value changes", () => {
+  const useValidated = createUseValidated((value) => {
+    return value.indexOf("a") >= 0;
+  });
+
+  let value = "apple";
+  const hook = renderHook(() => useValidated(value));
+
+  expect(hook.result.current).toBe("valid");
+
+  act(() => {
+    value = "blorgh";
+    hook.rerender();
+  });
+
+  expect(hook.result.current).toBe("invalid");
+
+  act(() => {
+    value = undefined;
+    hook.rerender();
+  });
+
+  expect(hook.result.current).toBe("idle");
+
+  act(() => {
+    value = "blabla";
+    hook.rerender();
+  });
+
+  expect(hook.result.current).toBe("valid");
+});
