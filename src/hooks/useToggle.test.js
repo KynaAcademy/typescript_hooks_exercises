7075e2b8e8ee7b1c5be01155cb7d7e5bd5f10d4e
Kelley van Evert <hello@kelleyvanevert.nl>
2020-10-27 11:12:39 +0100

    Added useToggle exercise

---
+++ src/hooks/useToggle.test.js
@@ -0,0 +1,38 @@
+import { renderHook, act } from "@testing-library/react-hooks";
+import { useToggle } from "./useToggle";
+
+test("returns the right data", () => {
+  const hook = renderHook(() => useToggle());
+
+  const [on1, toggle1] = hook.result.current;
+  expect(on1).toBe(false);
+  expect(typeof toggle1).toBe("function");
+});
+
+test("can toggle", () => {
+  const hook = renderHook(() => useToggle());
+
+  const [on1, toggle1] = hook.result.current;
+  expect(on1).toBe(false);
+
+  act(() => {
+    toggle1();
+  });
+
+  const [on2] = hook.result.current;
+  expect(on2).toBe(true);
+});
+
+test("toggle function is referentially stable", () => {
+  const hook = renderHook(() => useToggle());
+
+  const [, toggle1] = hook.result.current;
+
+  act(() => {
+    toggle1();
+  });
+
+  const [, toggle2] = hook.result.current;
+
+  expect(toggle1).toBe(toggle2);
+});
